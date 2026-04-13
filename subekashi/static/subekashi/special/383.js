0f8a49512dd7efafb7d0c07221c1d44f2ae5ca88
manmen2414 <inotitokokoro@gmail.com>
2026-04-13 23:19:42 +0900

    [Add] 雑スペシャルデザイン383の追加

---
+++ subekashi/static/subekashi/special/383.js
@@ -0,0 +1,134 @@
+function special() {
+    document.getElementsByClassName("dummybuttons")[0].remove();
+    
+    const lyricsEle = document.getElementById("lyrics");
+    lyricsEle.style.whiteSpace = "pre-wrap";
+    lyricsEle.innerHTML = `<style>no{opacity:0}</style>
+        年々得念連爺さん
+ある日、年々得念連爺さんは
+川にねんねんと
+くねんれんを
+ねねねねねねぶ
+えぷふじとふふんぎゅん
+にゅっぁぁぁぁっ！
+じゅっぽん、！！、
+…しに、川に芝刈り機を
+放り込みました。
+その頃ばあさんは、
+ねんねんばぶぶばばば
+→餅が詰まって死にかけている
+────そう、
+結局はいくらがんばっても
+人は死ぬんだ。
+僕たちには何も変えられない。
+そんなことを考えていたら、
+idea_0がiになった気がしました
+
+
+
+官途に就けた<no>光景</no>、
+それは<no>かんとの光景</no>。
+期せずして起こってしまったから、
+仕方が無い。
+鯖の<no>校門</no>に直腸を移植したので、
+肴にすれちまうね！
+鼻からソーメン飲みたい。
+
+ミラの<no>おにぎり</no>そのものと、
+ミラの<no>おにぎり</no>の中、
+ミラの<no>おにぎり</no>そのものと、
+ミラの<no>おにぎり</no>の外、
+ミラの<no>おにぎり</no>の先端、
+ミラの<no>おにぎり</no>の皮、
+ミラの<no>おにぎり</no>の前後、
+とっても
+Comprehensive。
+
+
+トキメキ<no>アカル</no>バスター
+人工<no>校門</no>ルート（RTA）
+ハエを舌で舐めるじゃにわいの
+ぺろぺろされたハエ。
+消防士裕二の<no>あがく</no>は、
+何処にある？此処にある？
+佐々木部長は駄目！
+ソース教えろ下さい。
+
+ミラの<no>おにぎり</no>そのものと、
+ミラの<no>おにぎり</no>の中、
+ミラの<no>おにぎり</no>そのものと、
+ミラの<no>おにぎり</no>の外、
+ミラの<no>おにぎり</no>の先端、
+ミラの<no>おにぎり</no>の皮、
+ミラの<no>おにぎり</no>の前後、
+とっても
+Supercalifragilisticexpi
+oooomoooooooooixaixaixa
+
+<no>かん元</no>フラクタルやめて
+ハエを<no>校門</no>にはさみたいね。
+全て(舛ﾛ牆犂の所為です。)
+<no>Sax</no>
+with
+Imaginary
+Friends（3P）
+
+
+Oooooooooooo
+Oooooooooooo
+
+
+
+
+
+
+
+
+
+Oooooooooooo
+Oooooooooooo
+Oooooooooooo
+Oooooooooooo
+Oooooooooooo
+Oooooooooooo
+Oooooooooooo
+
+
+
+Oooooooooooo
+
+
+
+ここまで流れに棹差したが、
+実はこれ歌詞<no>北ね</no>？
+もっと後難を恐れるべき？
+しかし、それが醍醐味。
+飛燕を見上げる蒼氓と、
+鄙びている雨音。
+嬰児見下げた中夕に、
+あなたの歔欷を聞いた。
+黎明が訪れた頃、
+ビエンナーレの物日が近付き、
+<no>おまえたち</no>が生えてきたw
+
+
+
+帰れ。
+    `
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    var defaultDummybuttonsEle =
+        document.getElementsByClassName("dummybuttons")[0];
+    const designedDummybuttonsEle = `
+    <div class="dummybuttons">
+            <div class="dummybutton" onclick="special()"><i class="fas fa-child"></i><p>あんしんモード</p></div>
+        <a href="./history/">
+            <div class="dummybutton"><i class="fas fa-history"></i><p>編集履歴</p></div>
+        </a>
+    </div>
+    `;
+    defaultDummybuttonsEle.innerHTML = stringToHTML(
+        designedDummybuttonsEle,
+    ).innerHTML;
+});
